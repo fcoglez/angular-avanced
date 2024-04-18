9fd397b50b84bb68115fb6c07d2f78fd9ae71121
Francisco Gonzalez <franciscogonzalezhontoria@gmail.com>
2024-04-18 12:04:20 +0200

    new service settings

--- src/app/pages/account-settings/account-settings.component.ts
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SettingsService } from '../../services/settings.service';
 
 
 @Component({
@@ -8,40 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountSettingsComponent implements OnInit{
 
-  public linkTheme =  document.querySelector('#theme');
-  public links!: NodeListOf<Element>;
-
-  constructor(){};
+  constructor(private settingService: SettingsService){};
 
   ngOnInit(): void {
-    this.links =  document.querySelectorAll('.selector');
-    this.changeCurrentTheme();
-  
+    this.settingService.checkCurrentTheme();
   }
 
   changeTheme(theme: string){
-   const url = `./assets/css/colors/${theme}.css`;
-   
-   this.linkTheme?.setAttribute('href', url);
-   localStorage.setItem('theme', url);
-   this.changeCurrentTheme()
+   this.settingService.changeTheme(theme);
   }
 
-  changeCurrentTheme(){
-    
-    this.links.forEach( element => {
-
-      element.classList.remove('working');
-
-      const btnColorSelected = element.getAttribute('data-theme');
-      const btnThemeUrl = `./assets/css/colors/${btnColorSelected}.css`;
-      const currentTheme = this.linkTheme?.getAttribute('href');
-
-      if (btnThemeUrl === currentTheme) {
-        element.classList.add('working');
-      }
-
-    });
-  }
+  
 
 }
--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,20 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { SettingsService } from '../services/settings.service';
 
 @Component({
   selector: 'app-pages',
   templateUrl: './pages.component.html',
   styles: ``
 })
-export class PagesComponent implements OnInit{
+export class PagesComponent {
 
-  public linkTheme =  document.querySelector('#theme');
+  constructor(private settingService: SettingsService){}
   
-  ngOnInit(): void {
   
-    const url = localStorage.getItem('theme') || './assets/css/colors/blue.css';
-    this.linkTheme?.setAttribute('href', url);
-    
-  }
 
 
 }
